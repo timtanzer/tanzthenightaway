e9188c10f21cd77476ea561f17abb373d111b079
timtanzer <timothytanzer@gmail.com>
2026-03-02 15:15:16 +1000

    Delete assets/js directory

--- assets/js/site.js
+++
@@ -1,9 +0,0 @@
-(function(){
-  const path = window.location.pathname.replace(/\/index\.html$/, '/');
-  document.querySelectorAll('nav a').forEach(a => {
-    const href = a.getAttribute('href');
-    if (!href || href.startsWith('http')) return;
-    const normalised = new URL(href, window.location.origin).pathname.replace(/\/index\.html$/, '/');
-    if (normalised === path) a.classList.add('active');
-  });
-})();
